f66b152316d642a437d749c0e421bbcabbd8c448
Ernesto Garcia <gnapse@gmail.com>
2014-09-30 10:46:02 -0300

    Remove unneeded code in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
